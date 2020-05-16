cdf34d30c6f3dc929a4c43c1912d33c6b94efaea
borna-blazevic <borna.blazevic@fer.hr>
2020-05-16 12:56:39 +0200

    add initial protobuf support

--- test.py
+++ test.py
@@ -7,8 +7,9 @@ import re
 import time
 import os
 import binascii
+from bitstring import BitArray
 import crcmod
-from proto.firmware_package_pb2 import *
+from models.firmware_package_pb2 import *
 
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 
@@ -18,7 +19,7 @@ if(len(sys.argv) < 2):
     print("USING DEFAULT LOCATION \"" , FIRMWARE_LOCATION, "\"")
 
 
-p1 = Popen('qemu-system-arm -machine lm3s6965evb -serial pty -kernel output/combined.bin -s',
+p1 = Popen('qemu-system-arm -machine lm3s6965evb -serial pty -kernel output/combined.bin -s -S',
           shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
 
 stdout_data = p1.stdout.readline()
@@ -31,32 +32,42 @@ print("Firmware size: ", fileSize)
 
 
 with open(pty, "wb+", buffering=0) as term:
-    print(term.readline().decode())
-    sys.stdout.flush()
-    term.write(fileSize.to_bytes(4,byteorder="little",signed=False))
+    sys.stdin.readline()
+    # print(term.readline().decode())
+    # sys.stdout.flush()
+    #term.write(fileSize.to_bytes(4,byteorder="little",signed=False))
     with open(os.path.join(CURRENT_PATH, FIRMWARE_LOCATION), "r") as f:
         print("Executing firmware upgrade")
 
-        
         while True:
-            packet = send_packet()
-            packet.line.record_type = f.read(2)
-            if(not packet.line.record_type):
+            srec_line = srec_line()
+            srec_line.record_type = f.read(2)
+            if(not srec_line.record_type):
                 break
-            packet.line.byte_count = f.read(2)
-            if(not packet.line.byte_count):
+            srec_line.byte_count = int(f.read(2), 16)
+            if(not srec_line.byte_count):
                 break
-            packet.line.address = f.read(4)
-            if(not packet.line.address):
+            srec_line.address = int(f.read(4), 16)
+            srec_line.data = binascii.a2b_hex(
+            f.read(2*srec_line.byte_count-6))
+            if(not srec_line.data):
                 break
-            packet.line.data = f.read(2*int(packet.line.byte_count, 16)-6)
-            if(not packet.line.data):
-                break
-            packet.line.checksum = f.read(2)
-            if(not packet.line.checksum):
+            srec_line.checksum = int(f.read(2), 16)
+            if(not srec_line.checksum):
                 break
 
-            print(packet.line.SerializeToString())
+            term.write(bytes([srec_line.ByteSize()]))
+            term.write(srec_line.SerializeToString())
+            print(term.read(1))
+            print(term.readline().decode(), end='')
+
+            while True:
+                print(term.readline().decode(), end='')
+            # print(term.readline().decode(), end='')
+            # print(term.readline().decode(), end='')
+            # print(term.readline().decode(), end='')
+            # print(term.readline().decode(), end='')
+            
             # term.write(byte)
             # res = term.readline().decode()
             # if(not (res.rstrip() =='Byte recieved' or res.rstrip() == 'Bytes written')):
