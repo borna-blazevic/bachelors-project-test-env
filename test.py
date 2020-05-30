08ff208bcea228a52cd69b43d978cdeb8e0009c7
borna-blazevic <borna.blazevic@fer.hr>
2020-05-30 21:08:14 +0200

    complete protobuf implementation of firmware upgrade

--- test.py
+++ test.py
@@ -8,9 +8,22 @@ import time
 import os
 import binascii
 from bitstring import BitArray
-import crcmod
-from models.firmware_package_pb2 import *
+from crccheck.crc import CrcBase
+import models.firmware_package_pb2 as firmware
 import serial
+import zlib
+
+
+class CrcSTM(CrcBase):
+    """CRC-STM"""
+    _width = 32
+    _poly = 0x4C11DB7
+    _initvalue = 0xFFFFFFFF
+    _reflect_input = False
+    _reflect_output = False
+    _xor_output = 0x0
+    _check_result = 0x376e6e7
+
 
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 
@@ -26,28 +39,63 @@ with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as term:
     with open(os.path.join(CURRENT_PATH, FIRMWARE_LOCATION), "r") as f:
         print("Executing firmware upgrade")
         last_line = 0
+        crc = CrcSTM()
+        checksum_data = 0
+        proceed = 0
         while True:
-
             p = term.readline()
-            while p != b'hi\n':
+            while True:
                 if p != b'':
-                    print(p)
+                    try:
+                        p = p[:-1]
+                        status_msg = firmware.status()
+
+                        status_msg.ParseFromString(p)
+                        if status_msg.status == firmware.status.status_enum.READY:
+                            proceed = 1
+                            break
+                        if status_msg.status == firmware.status.status_enum.ACK:
+                            proceed = 1
+                            break
+                        if status_msg.status == firmware.status.status_enum.REJECT:
+                            proceed = 0
+                            break
+
+                    except Exception as e:
+                        print(e)
+                        print(p)
                 p = term.readline()
 
-            line = srec_line()
-            line.record_type = f.read(2)
-            if(line.record_type == "S5"):
-                last_line = 1
-            line.byte_count = int(f.read(2), 16)
-            line.address = int(f.read(4), 16)
-            bla = f.read(2*line.byte_count-6)
-            line.data=(binascii.a2b_hex(bla))
-            line.checksum = int(f.read(2), 16)
-            term.write(bytes([line.ByteSize()]))
-            term.write(line.SerializeToString())
-            if (last_line):
-                break
-            f.read(1)
+            if proceed == 1:
+                if (last_line):
+                    break
+                packet = firmware.firmware_packet()
+                packet.line.record_type = f.read(2)
+                if(packet.line.record_type == "S5"):
+                    last_line = 1
+                packet.line.byte_count = int(f.read(2), 16)
+                packet.line.address = int(f.read(4), 16)
+                packet.line.data = (binascii.a2b_hex(
+                    f.read(2*packet.line.byte_count-6)))
+                packet.line.checksum = int(f.read(2), 16)
+                checksum_data = b'\x00\x00' + \
+                    bytes(packet.line.record_type, 'utf-8')
+                checksum_data = checksum_data + \
+                    packet.line.byte_count.to_bytes(4, byteorder='big')
+                checksum_data = checksum_data + \
+                    packet.line.address.to_bytes(4, byteorder='big')
+                checksum_data = checksum_data + \
+                    packet.line.checksum.to_bytes(4, byteorder='big')
+                if(not last_line == 1):
+                    checksum_data = checksum_data + packet.line.data
+                    while len(checksum_data) % 4:
+                        checksum_data = checksum_data + b'\x00'
+
+                packet.checksum = crc.calc(checksum_data)
+                f.read(1)
+
+            term.write(bytes([packet.ByteSize()]))
+            term.write(packet.SerializeToString())
 
         print("Finished sending firmware!")
         while True:
